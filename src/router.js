8c3f083da61acefcd4401faea572e335e17aa9c9
Alan <pollargithub@gmail.com>
2023-08-09 13:15:42 -0300

    atualizando rotas

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./routes/Home";
 import Registered from "./routes/Registered";
 import Error from "./routes/Error";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App/>,
